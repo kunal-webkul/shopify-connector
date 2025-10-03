3d21eb9b0440aa3272344a448e986bdc1ccc3325
Kunal kumar <111128645+kunal-webkul@users.noreply.github.com>
2025-10-03 11:11:59 +0530

    Update login.spec.js

--- tests/e2e-pw/tests/login.spec.js
+++ tests/e2e-pw/tests/login.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { login } from '../helpers/login';
 
 test.use({
-    browserName: 'firefox',
+    browserName: 'chromium',
     storageState: 'storage/auth.json', // Load session state
 });
 
